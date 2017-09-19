8672f4c2a86228a93dd387a8dc630fc374716c33
ahrobison <ahrob27@gmail.com>
2017-09-19 15:27:20 -0600

    Styled proposal-new page

--- app/proposal/proposal-new.component.ts
+++ app/proposal/proposal-new.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Proposal } from './proposal';
 
 @Component({
-	moduleId: module.id
-	selector: 'proposal-new'
-	templateUrl: 'proposal-new.component.html'
+	moduleId: module.id,
+	selector: 'proposal-new',
+	templateUrl: 'proposal-new.component.html',
+	stylesUrl: ['proposal-new.component.css']
 })
 
 export class ProposalNewComponent {
